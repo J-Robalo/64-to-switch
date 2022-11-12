f824e0066d52d900ce461f5348025f8f411a8e4e
João Robalo <joao27of@gmail.com>
2022-11-11 23:09:50 -0300

    Atualizando tabela e incrementando arquivo json com lista geral

---
+++ js/list.js
@@ -0,0 +1,13 @@
+const button = document.getElementsByClassName("botao-coluna");
+
+button.addEventListener("click", function () {
+  const colunaGeral = document.querySelector(".coluna-geral");
+
+  colunaGeral.classList.toggle("active");
+
+  if (colunaGeral.classList.contains("active")) {
+    return (button.textContent = "Return");
+  }
+
+  button.textContent = "Full list";
+});
